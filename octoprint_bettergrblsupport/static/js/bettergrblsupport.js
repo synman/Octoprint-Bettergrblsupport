a9b205f78be5dc0be0620b0b1214fa9d9c69ac26
Shell M. Shrader <shell@shellware.com>
2019-03-18 00:18:02 -0400

    Baseline

---
+++ octoprint_bettergrblsupport/static/js/bettergrblsupport.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Bettergrblsupport
+ *
+ * Author: Shell M. Shrader
+ * License: Apache 2.0
+ */
+$(function() {
+    function BettergrblsupportViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: BettergrblsupportViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_bettergrblsupport, #tab_plugin_bettergrblsupport, ...
+        elements: [ /* ... */ ]
+    });
+});
