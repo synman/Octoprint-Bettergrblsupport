87bff7ab5e8fe17a90544a793da1188d3197af61
Shell M. Shrader <shell@shellware.com>
2019-03-18 19:02:57 -0400

    updates

--- octoprint_bettergrblsupport/__init__.py
+++ octoprint_bettergrblsupport/__init__.py
@@ -158,7 +158,8 @@ class BettergrblsupportPlugin(octoprint.plugin.SettingsPlugin,
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-#__plugin_name__ = "Bettergrblsupport Plugin"
+
+__plugin_name__ = "Better Grbl Support"
 
 def __plugin_load__():
 	global __plugin_implementation__
