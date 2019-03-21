15d7f7479fe7f41348138df73bab9768f0635338
Shell M. Shrader <shell@shellware.com>
2019-03-20 23:52:09 -0400

    updates

--- octoprint_bettergrblsupport/__init__.py
+++ octoprint_bettergrblsupport/__init__.py
@@ -44,9 +44,9 @@ class BetterGrblSupportPlugin(octoprint.plugin.SettingsPlugin,
         self._settings.global_set(["serial", "helloCommand"], "M5")
         self._settings.global_set(["serial", "supportResendsWithoutOk"], "never")
 
-        self._logger.info("Setting defaults for UI elements tempTab=" + hideTempTab + " gCodeTab=" + hideGCodeTab)
-
         self._settings.save()
+
+        self._logger.info("Setting defaults for UI elements tempTab=%s gCodeTab=$s", hideTempTab, hideGCodeTab)
 
         return
 
