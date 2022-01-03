f5e228ac6345a532a154af87bf2236f28db0257f
Shell M. Shrader <shell@shellware.com>
2022-01-02 22:24:31 -0500

    touch version

--- octoprint_bettergrblsupport/static/js/bettergrblsupport.js
+++ octoprint_bettergrblsupport/static/js/bettergrblsupport.js
@@ -533,18 +533,6 @@ $(function() {
     $('#job_print')[0].innerHTML = "<i class=\"fas\" data-bind=\"css: {'fa-print': !isPaused(), 'fa-undo': isPaused()}\"></i> <span data-bind=\"text: (isPaused() ? 'Restart' : 'Start')\">Start</span>"
 
 
-    /* view model class, parameters for constructor, container to bind to
-     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-     * and a full list of the available options.
-     */
-    // OCTOPRINT_VIEWMODELS.push({
-    //     construct: BettergrblsupportViewModel,
-    //     // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-    //     dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
-    //     // Elements to bind to, e.g. #settings_plugin_bettergrblsupport, #tab_plugin_bettergrblsupport, ...
-    //     elements: [ /* ... */ ]
-    // });
-
     OCTOPRINT_VIEWMODELS.push([
       BettergrblsupportViewModel,
         [ "settingsViewModel", "loginStateViewModel" ],
