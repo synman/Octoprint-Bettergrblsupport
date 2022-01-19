36238863fce2928be48de2cab1bb8b4aa54f9bd2
Shell M. Shrader <shell@shellware.com>
2022-01-18 21:05:40 -0500

    fixed a typo on _settings.get

--- octoprint_bettergrblsupport/xyprobe.py
+++ octoprint_bettergrblsupport/xyprobe.py
@@ -48,8 +48,8 @@ class XyProbe:
         self._plugin._logger.debug("XyProbe: notify notifications=[{}] sessionId=[{}]".format(notifications, self._sessionId))
         self._step+=1
 
-        xProbeOffset = float(self._plugin._settings.get("xProbeOffset")) * self._plugin.invertX * -1
-        yProbeOffset = float(self._plugin._settings.get("yProbeOffset")) * self._plugin.invertY * -1
+        xProbeOffset = float(self._plugin._settings.get(["xProbeOffset"])) * self._plugin.invertX * -1
+        yProbeOffset = float(self._plugin._settings.get(["yProbeOffset"])) * self._plugin.invertY * -1
 
 
         for notification in notifications:
