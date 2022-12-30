ccc0fc361f3abf41dad3ccb7daf487933a3031ec
paukstelis <paukstel@umd.edu>
2022-12-29 20:36:30 -0500

    formating

--- octoprint_bettergrblsupport/__init__.py
+++ octoprint_bettergrblsupport/__init__.py
@@ -1398,7 +1398,7 @@ class BetterGrblSupportPlugin(octoprint.plugin.SettingsPlugin,
                 self._printer.commands("{}G91 G21 A{:f} F{}".format("$J=" if _bgs.is_grbl_one_dot_one(self) else "G1 ", distance * -1, zf))
             
             if direction == "b-right":
-                self._printer.commands("{}G91 G21 B{:f} F{}".format("$J=" if _bgs.is_grbl_one_dot_one(self) else "G1 ", distance * -1, zf))
+                self._printer.commands("{}G91 G21 B{:f} F{}".format("$J=" if _bgs.is_grbl_one_dot_one(self) else "G1 ", distance, zf))
             
             if direction == "b-left":
                 self._printer.commands("{}G91 G21 B{:f} F{}".format("$J=" if _bgs.is_grbl_one_dot_one(self) else "G1 ", distance * -1, zf))
