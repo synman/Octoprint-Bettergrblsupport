6af23599e0370af1cbad1b6f9d224eace9336160
Shell M. Shrader <shell@shellware.com>
2025-12-29 16:39:28 -0500

    Comment out command execution in PRINT_RESUMED event to prevent unintended actions

--- octoprint_bettergrblsupport/_bgs.py
+++ octoprint_bettergrblsupport/_bgs.py
@@ -367,14 +367,14 @@ def on_event(_plugin, event, payload):
     # Print Resumed
     if event == Events.PRINT_RESUMED:
         _plugin._logger.debug("resuming job")
-        send_command_now(_plugin._printer, _plugin._logger, [_plugin.grblMCode])
+        # send_command_now(_plugin._printer, _plugin._logger, [_plugin.grblMCode])
 
         # move our spindle back down 5
         if not is_laser_mode(_plugin):
             send_command_now(_plugin._printer, _plugin._logger, ["G4 P10", "G91 G0 Z-5"])
 
         # make sure we are using whatever positioning mode was active before we paused
-        send_command_now(_plugin._printer, _plugin._logger, ["G91" if _plugin.pausedPositioning == 1 else "G90"])
+        # send_command_now(_plugin._printer, _plugin._logger, ["G91" if _plugin.pausedPositioning == 1 else "G90"])
 
         _plugin.grblState = "Run"
         _plugin._plugin_manager.send_plugin_message(_plugin._identifier, dict(type="grbl_state", state="Run"))
