8dd513889a6a93e1dbc7934e11dda1826a768290
Shell M. Shrader <shell@shellware.com>
2025-12-30 21:23:11 -0500

    Bump wizard version to 20 and update release notes for version 2.3.3 with new features and improvements

--- octoprint_bettergrblsupport/__init__.py
+++ octoprint_bettergrblsupport/__init__.py
@@ -194,7 +194,7 @@ class BetterGrblSupportPlugin(octoprint.plugin.SettingsPlugin,
         self.bgsFilters = self.bgs_filters
 
         self.settingsVersion = 8
-        self.wizardVersion = 19
+        self.wizardVersion = 20
         
         self.whenConnected = time.monotonic()
         self.handshakeSent = False
