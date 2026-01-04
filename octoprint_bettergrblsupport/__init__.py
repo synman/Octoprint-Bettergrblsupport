50236667eaf5c9137f60b3e112dcd6bdeb8c89ca
Shell M. Shrader <shell@shellware.com>
2026-01-04 15:17:42 -0500

    prep 2.3.3rc3

--- octoprint_bettergrblsupport/__init__.py
+++ octoprint_bettergrblsupport/__init__.py
@@ -200,7 +200,7 @@ class BetterGrblSupportPlugin(octoprint.plugin.SettingsPlugin,
         self.bgsFilters = self.bgs_filters
 
         self.settingsVersion = 8
-        self.wizardVersion = 20
+        self.wizardVersion = 21
         
         self.whenConnected = time.monotonic()
         self.handshakeSent = False
