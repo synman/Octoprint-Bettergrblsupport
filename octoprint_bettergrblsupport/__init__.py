8950d96ba5ab14f05517fc6f5bc7415ee374f372
Shell M. Shrader <shell@shellware.com>
2026-01-06 13:01:36 -0500

    request config dump after reset if autoreporting

--- octoprint_bettergrblsupport/__init__.py
+++ octoprint_bettergrblsupport/__init__.py
@@ -1389,7 +1389,7 @@ class BetterGrblSupportPlugin(octoprint.plugin.SettingsPlugin,
         if command == "reset":
             _bgs.send_command_now(self._printer, self._logger, "M999")
             if _bgs.is_grbl_fluidnc(self) and self.fluidAutoReport:
-                threading.Thread(target=_bgs.send_command_now, kwargs={"printer": self._printer, "logger": self._logger, "cmd": "?", "waitTime": 2}).start()
+                threading.Thread(target=_bgs.send_command_now, kwargs={"printer": self._printer, "logger": self._logger, "cmd": "$CD", "waitTime": 2}).start()
             return
 
         if command == "updateGrblSetting":
